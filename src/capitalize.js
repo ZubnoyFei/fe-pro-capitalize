157ddfe8c25d056e5ea7aca09fe6bfddd2e2f9aa
ZubnoyFei <93076661+ZubnoyFei@users.noreply.github.com>
2021-12-20 16:35:56 +0200

    Update capitalize.js

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,5 +1,5 @@
 function capitalize(string) {let text = prompt(`Enter the result`);
                              let result = prompt.toUpperCase();
 }
-
+function capitalize(string);
 console.log(result);
