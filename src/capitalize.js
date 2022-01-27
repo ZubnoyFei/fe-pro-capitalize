4c05ccfe5e34f0f4038a4ebb7097e065db99e0ce
ZubnoyFei <93076661+ZubnoyFei@users.noreply.github.com>
2022-01-27 21:25:04 +0200

    Update capitalize.js

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,5 +1,18 @@
-function capitalize(string) {let text = prompt(`Enter the result`);
-                             let result = prompt.toUpperCase();
+const string = "lorem ipsum dolor sit amet";
+
+function capitalize(string) {
+
+    let text = string.split(' ');
+
+
+    const result = text.map(function (item) {
+        return item[0].toUpperCase() + item.slice(1);
+
+    });
+
+    return result.join(' ');
 }
-function capitalize(string);
-console.log(result);
+console.log(capitalize(string));
+
+
+module.exports = capitalize;
